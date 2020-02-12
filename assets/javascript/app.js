9ddf896b42fc1c8aa3241e030912eccfc93c88aa
Megan Cho <ymegan08@gmail.com>
2020-02-11 20:36:41 -0800

    added Azure sentiment analysis API within app.js and created config.js for setup and cognitives.js to store functions to call within app.js; still need to find a way to connect sentiment analysis with NYT API data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -62,18 +62,78 @@ function addArticles(array) {
 }
 
 
-// query for MS Sentiment Analysis API
-$.ajax({
-  url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.1/sentiment",
-  beforeSend: function(xhrObj){
-      // Request headers
-      xhrObj.setRequestHeader("Content-Type","application/json");
-      xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","{subscription key}"); // Add API key
-  },
-  type: "POST",
-  // Request body
-  data: "{body}",
-});
+// MS Sentiment Analysis API
+// Fetch wishes using axios
+const axios = require('axios');
+const {get_language, get_sentiments} = require('./cognitives');
+module.exports = async function (context, req) {
+    let dataUrl = "insert URL with array of objects to be used here";
+    try {
+        let getData = await axios.default.get(dataUrl);
+        let data = getData.data;
+      
+        // Get language
+        let languageDocuments = {
+            "documents" : []
+        }
+        
+        // Loop through and create an array of objects with the format {"id": id, text: "Text"}
+        for (let index = 0; index < data.length; index++) {
+            let text = data[index] && (data[index].message || data[index].mssage);
+            let document = {
+                id: index + 1,
+                text
+            }
+            languageDocuments.documents.push(document)
+        }
+
+        let getLanguage = await get_language(languageDocuments);
+        let language = getLanguage.data;
+
+        // Get sentiments
+        let sentimentDocuments = {
+            "documents" : []
+        }
+        
+        // Loop through and create an array of objects with the format {"id": id, lanuage: "language short form", text: "Text"}
+        for (let index = 0; index < data.length; index++) {
+            let text = data[index] && (data[index].message || data[index].mssage);
+            let document = {
+                id: index + 1,
+                language: language.documents && language.documents[index] 
+                            && language.documents[index].detectedLanguages[0].iso6391Name,
+                text
+            }
+            // Returns JSON response to be used for sentiment analysis
+            sentimentDocuments.documents.push(document)
+        }
+        let getSentiment = await get_sentiments(sentimentDocuments);
+        let sentiment = getSentiment.data;
+
+        // Classify as negative or positive
+        let sentimentList = []
+        for (let index = 0; index < data.length; index++) {
+            let who = data[index].who;
+            let state = sentiment.documents[index].score >= 0.5 ? 'Positive' : 'Negative';
+            let child = {
+                who, 
+                state
+            }
+            // Returns sentiment JSON response
+            sentimentList.push(child)
+        }
+        context.res = {
+            status: 200,
+            body: sentimentList
+        };
+    } catch (error) {
+        context.log(error)
+        context.res = {
+            status: 500,
+            body: "An error occurred - "+ error
+        };
+    }
+};
 
 // Example queryURL for Giphy API
 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
