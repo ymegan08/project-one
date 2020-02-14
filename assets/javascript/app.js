21d4ffd7ede00cf065338ab93779bdba589f98bf
Megan Cho <ymegan08@gmail.com>
2020-02-13 18:39:07 -0800

    changed sentiment analysis to twinword, need to add header parameter

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -63,77 +63,24 @@ function addArticles(array) {
 
 
 // MS Sentiment Analysis API
-// Fetch wishes using axios
-const axios = require('axios');
-const {get_language, get_sentiments} = require('./cognitives');
-module.exports = async function (context, req) {
-    let dataUrl = "insert URL with array of objects to be used here";
-    try {
-        let getData = await axios.default.get(dataUrl);
-        let data = getData.data;
-      
-        // Get language
-        let languageDocuments = {
-            "documents" : []
-        }
-        
-        // Loop through and create an array of objects with the format {"id": id, text: "Text"}
-        for (let index = 0; index < data.length; index++) {
-            let text = data[index] && (data[index].message || data[index].mssage);
-            let document = {
-                id: index + 1,
-                text
-            }
-            languageDocuments.documents.push(document)
-        }
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://twinword-sentiment-analysis.p.rapidapi.com/analyze/",
+	"method": "POST",
+	"headers": {
+		"x-rapidapi-host": "twinword-sentiment-analysis.p.rapidapi.com",
+		"x-rapidapi-key": "ee17b152f8msh2255cbce79fae02p1d4262jsn63775420543c",
+		"content-type": "application/x-www-form-urlencoded"
+	},
+	"data": {
+		"text": "enter text here!"
+	}
+}
 
-        let getLanguage = await get_language(languageDocuments);
-        let language = getLanguage.data;
-
-        // Get sentiments
-        let sentimentDocuments = {
-            "documents" : []
-        }
-        
-        // Loop through and create an array of objects with the format {"id": id, lanuage: "language short form", text: "Text"}
-        for (let index = 0; index < data.length; index++) {
-            let text = data[index] && (data[index].message || data[index].mssage);
-            let document = {
-                id: index + 1,
-                language: language.documents && language.documents[index] 
-                            && language.documents[index].detectedLanguages[0].iso6391Name,
-                text
-            }
-            // Returns JSON response to be used for sentiment analysis
-            sentimentDocuments.documents.push(document)
-        }
-        let getSentiment = await get_sentiments(sentimentDocuments);
-        let sentiment = getSentiment.data;
-
-        // Classify as negative or positive
-        let sentimentList = []
-        for (let index = 0; index < data.length; index++) {
-            let who = data[index].who;
-            let state = sentiment.documents[index].score >= 0.5 ? 'Positive' : 'Negative';
-            let child = {
-                who, 
-                state
-            }
-            // Returns sentiment JSON response
-            sentimentList.push(child)
-        }
-        context.res = {
-            status: 200,
-            body: sentimentList
-        };
-    } catch (error) {
-        context.log(error)
-        context.res = {
-            status: 500,
-            body: "An error occurred - "+ error
-        };
-    }
-};
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
 
 // Example queryURL for Giphy API
 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
