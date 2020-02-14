7ef4b80a8cddd644238a1b1fed1fae17d8a4e56a
subtleasian <danielmson21@gmail.com>
2020-02-13 19:21:43 -0800

    started integrating the NYT and Sentiment Analysis API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,12 +24,15 @@ $(document).on("click", "#topic-button", function() {
     var topic = $(this).attr("data-element");
     var queryNYT = "https://api.nytimes.com/svc/topstories/v2/" + topic + ".json?api-key=" + apiKey;
 
-    console.log($(this).attr("data-element"));    
+    console.log();    
 
     $.ajax({
         url: queryNYT,
         method: "GET"
     }).then(function(response){
+        console.log("NYT API worked");
+        
+
         abstractArr = [];
         titleArr = [];
         linkArr = [];
@@ -42,56 +45,65 @@ $(document).on("click", "#topic-button", function() {
         }
     
         console.log(abstractArr, titleArr, linkArr);
-        addArticles(titleArr);
-    
+        
+
+        // Twinword Sentiment Analysis API
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://twinword-sentiment-analysis.p.rapidapi.com/analyze/",
+            "method": "POST",
+            "headers": {
+                "x-rapidapi-host": "twinword-sentiment-analysis.p.rapidapi.com",
+                "x-rapidapi-key": "ee17b152f8msh2255cbce79fae02p1d4262jsn63775420543c",
+                "content-type": "application/x-www-form-urlencoded"
+            },
+            "data": {
+                // Give this value of information from NYT API
+                "text": ""
+            }
+        }
+        // Call Twinword API for every string sent to it
+        for(var i = 0; i < abstractArr.length; i++){
+            settings.data.text = abstractArr[i];
+            $.ajax(settings).done(function (response) {
+                // Extract positive, neutral, negative string to add too emotion coloumn
+                var sentimentAnalysis = response.type;
+                // feed GIPHY API response.type string
+                // have GIPHY shown on the columns
+
+                addItems(arr1, sentimentAnalysis, arr3)
+            });
+        }
+
+
     });
+    
 });
+
+
 
 // add Articles to DOM
-function addArticles(array) {
-    for (i = 0; i < array.length; i++) {
-        var tRow = $("<tr>");
-        var articleTd = $("<td>").text(array[i]);
-        var emotionTd = $("<td>");
-        var giphyTd = $("<td>");
-        
-        tRow.append(articleTd, emotionTd, giphyTd);
-        
-        $("tbody").append(tRow);
-    }
+// function addItems(NYTAbstract, SentimentAnalysis, GiphyLink) {
+    
+    var tRow = $("<tr>");
+    var articleTd = $("<td>").text(array[i]);
+    var emotionTd = $("<td>").text(SentimentAnalysis);
+    // var giphyTd = $("<td>");
+    
+    tRow.append(articleTd, emotionTd, giphyTd);
+    
+    $("tbody").append(tRow);
 }
 
 
-// MS Sentiment Analysis API
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://twinword-sentiment-analysis.p.rapidapi.com/analyze/",
-	"method": "POST",
-	"headers": {
-		"x-rapidapi-host": "twinword-sentiment-analysis.p.rapidapi.com",
-		"x-rapidapi-key": "ee17b152f8msh2255cbce79fae02p1d4262jsn63775420543c",
-		"content-type": "application/x-www-form-urlencoded"
-	},
-	"data": {
-        // Give this value of information from NYT API
-		"text": "enter text here!"
-	}
-}
-// Console logs response A.K.A sentiment analysis
-// Type response: negative, neutral, positive
-// score: how negative or positive the overall text analyzed is
-    // Below -0.05 is negative, above 0.05 is positive
-// ratio: combined total score of negative words compared to combined total score of positive words (-1 to 1)
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
-
-// Example queryURL for Giphy API
+//Example queryURL for Giphy API
 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
 var data = null;
 
-$.ajax({
+function getGiphy() = {}
+
+    $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
--- assets/javascript/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // Setup with keys
-    TEXT_ANALYTICS_SUBSCRIPTION_KEY: "3fa9a825088444a1b6c197c9961e2883",
-    TEXT_ANALYTICS_ENDPOINT: "https://yunacho.cognitiveservices.azure.com/text/analytics"
-}
