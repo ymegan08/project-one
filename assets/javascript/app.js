a1c1a5a040e831dbb65d37620cb56993a09bcc48
iamjink <jinijinjin@gmail.com>
2020-02-15 13:55:14 -0800

    showing three different headlines with new nytArr

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,7 +19,7 @@ function showButtons() {
 
 // query for NYT API
 
-$(document).on("click", "#topic-button", function() {
+$(document).on("click", "#topic-button", function () {
     var apiKey = "UA8uSAgssGj8XdWmpw2aN3UOEEBviYiJ";
     var topic = $(this).attr("data-element");
     var queryNYT = "https://api.nytimes.com/svc/topstories/v2/" + topic + ".json?api-key=" + apiKey;
@@ -27,84 +27,93 @@ $(document).on("click", "#topic-button", function() {
     $.ajax({
         url: queryNYT,
         method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
         console.log("NYT API worked");
-        
+
 
         abstractArr = [];
         titleArr = [];
         linkArr = [];
         $("tbody tr").remove();
-    
+
         for (const item in response.results) {
             abstractArr.push(response.results[item].abstract);
-            titleArr.push(response.results[item].title);
-            linkArr.push(response.results[item].url);
+            // titleArr.push(response.results[item].title);
+            // linkArr.push(response.results[item].url);
         }
-        
+
+        nytArr = [];
+
+        nytArr.push(abstractArr[0], abstractArr[1], abstractArr[2]);
+
+        console.log(nytArr);
+
         // Call Twinword API for every string sent to it
-        
-            
-        // Twinword Sentiment Analysis API
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://twinword-sentiment-analysis.p.rapidapi.com/analyze/",
-            "method": "POST",
-            "headers": {
-                "x-rapidapi-host": "twinword-sentiment-analysis.p.rapidapi.com",
-                "x-rapidapi-key": "ee17b152f8msh2255cbce79fae02p1d4262jsn63775420543c",
-                "content-type": "application/x-www-form-urlencoded"
-            },
-            "data": {
-                // Give this value of information from NYT API
-                "text": ""
+        for (let i = 0; i < nytArr.length; i++) {
+            // Twinword Sentiment Analysis API
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://twinword-sentiment-analysis.p.rapidapi.com/analyze/",
+                "method": "POST",
+                "headers": {
+                    "x-rapidapi-host": "twinword-sentiment-analysis.p.rapidapi.com",
+                    "x-rapidapi-key": "ee17b152f8msh2255cbce79fae02p1d4262jsn63775420543c",
+                    "content-type": "application/x-www-form-urlencoded"
+                },
+                "data": {
+                    // Give this value of information from NYT API
+                    "text": nytArr[i]
+                }
             }
-        }
 
-        
-        settings.data.text = abstractArr[0];
-        console.log(settings);
-        
-        $.ajax(settings).done(function(response) {
-            // Extract positive, neutral, negative string to add too emotion coloumn
-            var sentimentAnalysis = response.type
-            var keywords = response.keywords[0].word;
-            console.log(response)
-            // feed GIPHY API response.type string
-            var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=" + keywords;
-            var GiphyLink;
 
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function (response) {
-                var abstractText = settings.data.text
-                console.log("Giphy API Worked");
-                // store image link to GiphyLink
-                GiphyLink = response.data.images.downsized_large.url;
-                // have GIPHY shown on the columns
-                addItems(abstractText, sentimentAnalysis, GiphyLink);
+         
+            console.log(settings);
+
+            $.ajax(settings).done(function (response) {
+                // Extract positive, neutral, negative string to add too emotion coloumn
+                var sentimentAnalysis = response.type
+                var keywords = response.keywords[i].word;
+                console.log(response)
+                // feed GIPHY API response.type string
+                var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=" + keywords;
+                var GiphyLink;
+
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function (response) {
+                    var abstractText = settings.data.text
+                    console.log("Giphy API Worked");
+                    // store image link to GiphyLink
+                    GiphyLink = response.data.images.downsized_large.url;
+                    // have GIPHY shown on the columns
+                    addItems(nytArr[i], sentimentAnalysis, GiphyLink);
+                });
+
             });
 
-        });       
+        }
+
+
 
     });
-    
+
 });
 
 
 
 // add Articles to DOM
 function addItems(string, SentimentAnalysis, GiphyLink) {
-    
+
     var tRow = $("<tr>");
     var articleTd = $("<td>").text(string);
     var emotionTd = $("<td>").text(SentimentAnalysis);
     var giphyTd = $("<td>").html("<img src=" + GiphyLink + ">");
-    
+
     tRow.append(articleTd, emotionTd, giphyTd);
-    
+
     $("tbody").append(tRow);
 }
 
